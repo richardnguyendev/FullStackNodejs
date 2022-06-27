0df06a1e334629c4a217a8db35e19069092c405c
Richard Nguyen <vinh.thanhdemon@gmail.com>
2022-06-27 19:25:36 +0700

    first commit

---
+++ src/controllers/homeController.js
@@ -0,0 +1,13 @@
+
+let getHomePage = (req,res)=>{
+    return res.render('homepage.ejs');
+}
+
+let getAboutpage = (req,res)=>{
+    return res.render('test/about.ejs');
+}
+
+module.exports={
+    getHomePage: getHomePage,
+    getAboutpage:getAboutpage,
+}
---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let app = express();
+
+//config app
+app.use (bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+viewEngine(app);
+initWebRoutes(app);
+
+let port = process.env.PORT || 6969;
+//Port ==== underfined => port = 6969
+
+app.listen(port, () =>{
+    //callback
+    console.log("backend Nodejs is running on the port: " + port)
+});
